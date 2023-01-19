72ef741b81585ba3884a42f4601da406d73be3c9
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-19 17:10:07 +0000

    Navbar

--- src/components/HomeNavbar.jsx
+++ src/components/HomeNavbar.jsx
@@ -12,20 +12,19 @@ function HomeNavbar() {
           src="./Pinterest-logo.png"
           className="h-10 sm:h-20 ml-3"
           alt="Pinterest-logo"
-          srcset=""
         />
 
         <div className="flex justify-around w-[50%] md:w-3/12">
           <button
             onClick={() => setLoginModal(true)}
-            className="bg-[#e60023] text-white font-normal md:font-semibold
+            className="text-sm bg-[#e60023] text-white font-normal md:font-semibold
              rounded-2xl py-1 px-2 sm:py-2 sm:px-4 hover:bg-[#ad081b]"
           >
             Log in{" "}
           </button>
           <button
             onClick={() => setSignUpModal(true)}
-            className="bg-[#e1e1e1] font-normal md:font-semibold rounded-2xl 
+            className="text-sm bg-[#e1e1e1] font-normal md:font-semibold rounded-2xl 
              py-1 px-2 sm:py-2 sm:px-4 hover:bg-[#efefef]"
           >
             Sign Up
