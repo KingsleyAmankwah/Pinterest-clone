de98e232355df02a5667f7d3441c607fdf35c937
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-19 17:09:10 +0000

    api

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseUrl: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID q2Yl_r9cTaOQPb7IGvylE8mWMuaU6ViYwNV-2XiD6pk",
+    Authorization: `Client-ID ${process.env.REACT_APP_API_KEY}`,
   },
 });
