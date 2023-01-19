3518546d300cc3fd6cd9be49408b7e5c9052d709
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-19 17:09:56 +0000

    Home board

--- src/components/HomeDashboard.jsx
+++ src/components/HomeDashboard.jsx
@@ -13,113 +13,113 @@ function HomeDashboard() {
       </div>
 
       <div>
-        <div className="flex flex-col items-center overflow-hidden mt-3">
+        <div className="flex flex-col items-center overflow-hidden">
           <div className="grid grid-cols-7 gap-[252px]">
             <div className="column flex flex-col items-center w-full mt-0">
               <div className="item">
-                <img src="./c1r1.jpg" alt="" srcset="" />
+                <img src="./c1r1.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c1r2.jpg" alt="" srcset="" />
+                <img src="./c1r2.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c1r3.jpg" alt="" srcset="" />
+                <img src="./c1r3.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c1r4.jpg" alt="" srcset="" />
+                <img src="./c1r4.jpg" alt="" />
               </div>
             </div>
 
             <div className="column flex flex-col items-center w-full mt-[160px]">
               <div className="item">
-                <img src="./c2r1.jpg" alt="" srcset="" />
+                <img src="./c2r1.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c2r2.jpg" alt="" srcset="" />
+                <img src="./c2r2.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c2r3.jpg" alt="" srcset="" />
+                <img src="./c2r3.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c2r4.jpg" alt="" srcset="" />
+                <img src="./c2r4.jpg" alt="" />
               </div>
             </div>
 
             <div className="column flex flex-col items-center w-full mt-[320px]">
               <div className="item">
-                <img src="./c3r1.jpg" alt="" srcset="" />
+                <img src="./c3r1.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c3r2.jpg" alt="" srcset="" />
+                <img src="./c3r2.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c3r3.jpg" alt="" srcset="" />
+                <img src="./c3r3.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c3r4.jpg" alt="" srcset="" />
+                <img src="./c3r4.jpg" alt="" />
               </div>
             </div>
 
             <div className="column flex flex-col items-center w-full mt-[480px]">
               <div className="item">
-                <img src="./c4r1.jpg" alt="" srcset="" />
+                <img src="./c4r1.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c4r2.jpg" alt="" srcset="" />
+                <img src="./c4r2.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c4r3.jpg" alt="" srcset="" />
+                <img src="./c4r3.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c4r4.jpg" alt="" srcset="" />
+                <img src="./c4r4.jpg" alt="" />
               </div>
             </div>
 
             <div className="column flex flex-col items-center w-full mt-[320px]">
               <div className="item">
-                <img src="./c5r1.jpg" alt="" srcset="" />
+                <img src="./c5r1.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c5r2.jpg" alt="" srcset="" />
+                <img src="./c5r2.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c5r3.jpg" alt="" srcset="" />
+                <img src="./c5r3.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c5r4.jpg" alt="" srcset="" />
+                <img src="./c5r4.jpg" alt="" />
               </div>
             </div>
 
             <div className="column flex flex-col items-center w-full mt-[160px]">
               <div className="item">
-                <img src="./c6r1.jpg" alt="" srcset="" />
+                <img src="./c6r1.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c6r2.jpg" alt="" srcset="" />
+                <img src="./c6r2.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c6r3.jpg" alt="" srcset="" />
+                <img src="./c6r3.jpg" alt="" />
               </div>
               <div className="item">
-                <img src="./c6r4.jpg" alt="" srcset="" />
+                <img src="./c6r4.jpg" alt="" />
               </div>
             </div>
 
             <div className="column flex flex-col items-center w-full mt-0">
               <div className="item">
-                <img src="./c6r4.jpg" alt="" srcset="" />
+                <img src="./c6r4.jpg" alt="" />
               </div>
 
               <div className="item">
-                <img src="./c5r4.jpg" alt="" srcset="" />
+                <img src="./c5r4.jpg" alt="" />
               </div>
 
               <div className="item">
-                <img src="./c4r4.jpg" alt="" srcset="" />
+                <img src="./c4r4.jpg" alt="" />
               </div>
 
               <div className="item">
-                <img src="./c7r4.jpg" alt="" srcset="" />
+                <img src="./c7r4.jpg" alt="" />
               </div>
             </div>
           </div>
